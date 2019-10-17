251c0be9f8af511247db70dcff1559db83ce0461
Tuomas Östman <tuomas.ostman@hotmail.fi>
2019-10-17 13:50:45 +0300

    First small test queries for EF6 and Dapper

--- App/Program.cs
+++ App/Program.cs
@@ -1,15 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using FrameworkApp.Entities;
+using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Dapper;
 
 namespace App
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly string _connectionString = @"data source=OSTMYLLY\SQLEXPRESS;initial catalog=Thesis;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+
+        static void Main()
         {
+            var eFStopwatch = new Stopwatch();
+
+            // EntityFramework
+            eFStopwatch.Start();
+
+            Console.WriteLine("Fetchin data With EntityFramework!\n");
+
+            var EFResult = GetDataEF();
+
+            Console.WriteLine("Result was: \n");
+
+            Console.WriteLine(EFResult + "\n");
+
+            eFStopwatch.Stop();
+
+            Console.WriteLine("Time Elapsed: " + eFStopwatch.Elapsed + "\n");
+
+            Console.WriteLine("\n------------------------------------\n");
+
+            //Dapper
+
+            var dapperStopwatch = new Stopwatch();
+            dapperStopwatch.Start();
+
+            Console.WriteLine("Fetchin data With Dapper!\n");
+
+            var DapperResult = GetDataDapper();
+            Console.WriteLine("Result was: \n");
+
+            Console.WriteLine(DapperResult + "\n");
+
+            dapperStopwatch.Stop();
+
+            Console.WriteLine("Time Elapsed: " + dapperStopwatch.Elapsed + "\n");
+
+            Console.WriteLine("Press any key to close this window.");
+            Console.ReadKey();
+        }
+
+        static string GetDataEF()
+        {
+            try
+            {
+
+                using (var db = new ThesisEntities())
+                {
+                    return (from tt in db.TestTable
+                            where tt.ID == 1
+                            select tt.Data).FirstOrDefault().ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception occured: " + ex);
+                return ex.Message;
+            }
+        }
+        static string GetDataDapper()
+        {
+            try
+            {
+                using (var conn = new SqlConnection(_connectionString))
+                {
+                    return conn.Query<string>("SELECT Data FROM TestTable WHERE ID = 1").FirstOrDefault();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception occured: " + ex);
+                return ex.Message;
+            }
         }
     }
 }
